7dbaa74e55329853492ac039427a5f2e54f2ff89
Aleja955 <alejapaiva97@gmail.com>
2025-04-08 17:53:16 -0500

    Agregando JS

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+let colorEstado = 0;
 
+function cambiarColor() {
+    const titulo = document.getElementById("titulo");
+
+    if (colorEstado === 0) {
+        titulo.style.color = "tomato";
+        colorEstado = 1;
+    } else if (colorEstado === 1) {
+        titulo.style.color = "yellow";
+        colorEstado = 2;
+    } else {
+        titulo.style.color = "green";
+        colorEstado = 0;
+    }
+}
 
 
 
